b4841122f716e8f65135f70f769e58ad58be1a5b
liumh <lmh@cloud-open.cn>
2012-05-18 21:54:25 +0800

    refs #12, 配置文件中定义Nova Compute结点

--- cloud_monitor_settings.py
+++ cloud_monitor_settings.py
@@ -1,9 +1,11 @@
 
 # configuration for cloud_monitor and etc...
 db_engine = 'mysql'
-db_server = '172.16.0.171'
+db_server = '10.0.200.6'
 db_username = 'root'
-db_password ='root'
+db_password ='cloudmonitor'
 db_database = 'cloud_monitor'
 
 api_server_port = 8080
+
+nova_compute_noe = ['10.0.200.1']
