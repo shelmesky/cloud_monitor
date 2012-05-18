5bfb460691965c14ea0a6ad9c18fb05b016bd135
root <root@cloudmonitor.(none)>
2012-05-18 22:10:43 +0800

    refs #12, 完善过期实例和token的检查功能

--- check_expired.py
+++ check_expired.py
@@ -64,9 +64,10 @@ def _check_expired_cloud_token():
         for line in lines:
             token = line['token']
             last_use_time = line['last_use_time']
-            if last_use_time: no_active_time = int(now_date) - int(last_use_time)
+            if not last_use_time: last_use_time = 0
+            no_active_time = int(now_date) - int(last_use_time)
             if no_active_time >= 720:
-                _db.delete(tokens.tokens_table,where="`token`='%s'" % token)
+                _db.delete(table,where="`token`='%s'" % token)
     except:
         err = trace()
         logger.error(err)
